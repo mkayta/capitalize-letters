0ac742f03f069b4386ae785feeceb3dbb8edc5cc
Eng-Mohamed <mkayta2023@gmail.com>
2024-10-01 19:54:23 +0300

    I done this Challenge

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Good Luck 💪🏾
+function makeCapital(word){
+let wordsplit=word.split(" ");
+let wordcapital=wordsplit.map(str => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+});
+let wordjoin=wordcapital.join(" ");
+return wordjoin
+
+}
+
+console.log(makeCapital("my name is ali")) 
+console.log(makeCapital("fikrcamp Bootcamp")) 
+console.log(makeCapital("i live In Paris")) 
